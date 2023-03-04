783c3a82f3ed820e5714d5502c4ac706b1c248f0
Haadiiii <hammali99@gmail.com>
2023-03-04 20:25:50 +0500

    create bike POST, GET, PUT, DELETE methods

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify;
 from flask_cors import CORS;
 from pymongo import MongoClient;
 import datetime;
-
+from bson.objectid import ObjectId;
 app = Flask(__name__)
 CORS(app)
 
@@ -58,6 +58,107 @@ def auth_user():
         return jsonify({'token': token.decode('UTF-8')})
     else:
         return jsonify({'message': 'Invalid email or password!'})
+
+
+@app.route('/api/mybikes', methods=['POST'])
+def create_bike():
+    # check if bike already exists
+    existing_bike = db.bikes.find_one({'name': request.json['name']})
+    if existing_bike:
+      return jsonify({'message': 'Bike already exists!'}), 400
+    else:
+        db.bikes.insert_one({
+            'name': request.json['name'],
+            'bike_type': request.json['bike_type'],
+            'images':  request.json['images'],
+            'description': request.json['description'],
+            'daily_rate': request.json['daily_rate'],
+            'brand': request.json['brand'],
+            'color': request.json['color'],
+        })
+        return jsonify({'message': 'Bike created successfully!'}), 201
+
+
+@app.route('/api/mybikes', methods=['GET'])
+def get_bikes():
+    bikes = []
+    for doc in db.bikes.find():
+        bikes.append({
+            'id': str(doc['_id']),
+            'name': doc['name'],
+            'bike_type': doc['bike_type'],
+            'images': doc['images'],
+            'description': doc['description'],
+            'daily_rate': doc['daily_rate'],
+            'brand': doc['brand'],
+            'color': doc['color'],
+        })
+    return jsonify({'bikes': bikes}), 200
+
+@app.route('/api/mybikes/<id>', methods=['GET'])
+def get_bike(id):
+    bike = db.bikes.find_one({'_id': ObjectId(id)})
+    return jsonify({
+        'id': str(bike['_id']),
+        'name': bike['name'],
+        'bike_type': bike['bike_type'],
+        'images': bike['images'],
+        'description': bike['description'],
+        'daily_rate': bike['daily_rate'],
+        'brand': bike['brand'],
+        'color': bike['color'],
+    })
+
+# update bike
+@app.route('/api/mybikes/<id>', methods=['PUT'])
+def update_bike(id):
+    db.bikes.update_one({'_id': ObjectId(id)}, {'$set': {
+        'name': request.json['name'],
+        'bike_type': request.json['bike_type'],
+        'images':  request.json['images'],
+        'description': request.json['description'],
+        'daily_rate': request.json['daily_rate'],
+        'brand': request.json['brand'],
+        'color': request.json['color'],
+    }})
+    return jsonify({'message': 'Bike updated successfully!'}), 200
+
+# delete bike
+@app.route('/api/mybikes/<id>', methods=['DELETE'])
+def delete_bike(id):
+    db.bikes.delete_one({'_id': ObjectId(id)})
+    return jsonify({'message': 'Bike deleted successfully!'}), 200
+
+# create bike reservation from bike id
+@app.route('/api/mybikes/<id>/reservations', methods=['POST'])
+def create_bike_reservation(id):
+    # check if bike already exists
+    existing_bike = db.bikes.find_one({'_id': ObjectId(id)})
+    if existing_bike:
+        db.reservations.insert_one({
+            'bike_id': id,
+            'resevation_date': request.json['resevation_date'],
+            'due_date': request.json['due_date'],
+            'city': request.json['city'],
+        })
+        return jsonify({'message': 'Bike reservation created successfully!'}), 201
+    else:
+        return jsonify({'message': 'Bike does not exist!'}), 400
+
+
+@app.route('/api/mybikes/reservations', methods=['GET'])
+
+def get_bike_reservations():
+    reservations = []
+    for doc in db.reservations.find():
+        reservations.append({
+            'id': str(doc['_id']),
+            # 'resevation_date': doc['resevation_date'],
+            'due_date': doc['due_date'],
+            'city': doc['city'],
+        })
+    return jsonify({'reservations': reservations}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
